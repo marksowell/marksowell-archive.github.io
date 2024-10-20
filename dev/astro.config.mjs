b5936d6bab7ad76d85d329d87c37d98da3d6bccd
Mark Sowell <mark@marksowell.com>
2024-10-20 15:11:16 -0600

    .

--- dev/astro.config.mjs
+++ dev/astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://marksowell.com', // Your domain
-  base: '/dev',                   // Serves the Astro site under /dev
+  base: '/dev',                  // Serves the Astro site under /dev
+  root: './dev',
+  srcDir: './dev',
+  publicDir: './dev'
 });
